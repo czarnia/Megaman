a93bdcd52080cd39e416af96f66527da3dd32f13
Ana Czarnitzki <czarniana@gmail.com>
2016-05-13 15:03:47 -0300

    Agrego mi ventanita al cliente

--- src/Cliente/client.cpp
+++ src/Cliente/client.cpp
@@ -1,5 +1,6 @@
 #include "socket.h"
 
+#include <gtkmm.h>
 #include <sstream>
 #include <iostream>
 #include <cstdlib>
@@ -9,6 +10,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+
+void on_button_clicked1(){
+  std::cout << "Me hicieron un click (1)!" << std::endl;
+}
+
+void on_button_clicked2(){
+  std::cout << "Me hicieron un click (2)!" << std::endl;
+}
 
 int main(int argc, char *argv[]){
   char* id = argv[1];
@@ -23,6 +32,30 @@ int main(int argc, char *argv[]){
   }
 
   //Aca va el ciclo del cliente
+
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_title("Megaman");
+  window.set_default_size(200, 200);
+
+  Gtk::Box *vbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL, 0));
+  window.add(*vbox);
+
+  Gtk::Button boton1("Click 1");
+  Gtk::Button boton2("Click 2");
+
+  boton1.signal_clicked().connect(sigc::ptr_fun(&on_button_clicked1));
+  boton2.signal_clicked().connect(sigc::ptr_fun(&on_button_clicked2));
+
+  (*vbox).add(boton1);
+  (*vbox).add(boton2);
+
+  vbox->show_all();
+
+  app->run(window);
+
+  //
 
   (*cliente).shutdown(SHUT_RDWR);
   delete cliente;
