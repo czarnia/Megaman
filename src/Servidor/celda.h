0d7a5c5904bbfd42e290b331cea06d99bb2a3a45
Ana Czarnitzki <czarniana@gmail.com>
2016-05-21 15:08:42 -0300

    Agrego los métodos de celdas adyacentes a celda

--- src/Servidor/celda.h
+++ src/Servidor/celda.h
@@ -1,9 +1,19 @@
 #ifndef CELDA_H
 #define CELDA_H
 
+#include "coordenada.h"
+
 class Celda{
   public:
     virtual bool puedo_ubicar() = 0;
+    virtual Coordenada celda_derecha() = 0;
+    virtual Coordenada celda_izquierda() = 0;
+    virtual Coordenada celda_arriba() = 0;
+    virtual Coordenada celda_abajo() = 0;
+    virtual Coordenada celda_diagonal_derabajo() = 0;
+    virtual Coordenada celda_diagonal_derarriba() = 0;
+    virtual Coordenada celda_diagonal_izqabajo() = 0;
+    virtual Coordenada celda_diagonal_izqarriba() = 0;
     virtual ~Celda();
 };
 
