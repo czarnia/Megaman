4b79e50f205dc0b7ec6175f89822c030a908ef2e
Ana Czarnitzki <czarniana@gmail.com>
2016-05-21 17:25:49 -0300

    Agrego nueva base del mapa

--- src/Servidor/mapa.cpp
+++ src/Servidor/mapa.cpp
@@ -1,54 +1,57 @@
 #include "mapa.h"
 #include "coordenada.h"
+#include "celda_aire.h"
+#include "celda_tierra.h"
+#include "elemento.h"
+#include "puas.h"
+#include "escalera.h"
 
-Mapa::Mapa(){
-  //mapa.cargar();
+Mapa::Mapa(size_t tamanio){
+	tam = tamanio;
+  mapa.cargar();
 }
 
-bool Mapa::ocupar(Ubicable &nuevo_ubicable, std::vector<Coordenada*> &coordenadas){
-	Celda posicion;
-	size_t x, y;
-	//Antes de ocupar un conjunto de celdas verifico que el
-	//la operacion es válida:
-	for (size_t pos = 0; pos < coordenadas.size(); pos++){
-		x = coordenadas[pos].obtener_abscisa();
-		y = coordenadas[pos].obtener_ordenada();
-		posicion = celdas[x][y];
-		if (!posicion.puede_ocupar(nuevo_ubicable)){
-			return false;
-		}
-	}
-	//Ocupo las celdas:
-	for (size_t pos = 0; pos < coordenadas.size(); pos++){
-		x = coordenadas[pos].obtener_abscisa();
-		y = coordenadas[pos].obtener_ordenada();
-		posicion = celdas[x][y];
-		if (!posicion.ocupar(nuevo_ubicable)){
-			return false; //desocupar celdas ocupadas...lanzar excepcion... (?)
-		}
-	}
-	return true;
-}
-
-void Mapa::desocupar(std::vector<Coordenada*> &coordenadas){
-	size_t x, y = 0;
-	for (size_t pos = 0; pos < coordenadas.size(); pos++){
-		x = coordenadas[pos].obtener_abscisa();
-		y = coordenadas[pos].obtener_ordenada();
-		Celda posicion = celdas[x][y];
-		posicion.desocupar();
+void Mapa::ocupar_elemento(Elemento& elem, std::vector<Coordenada> coordenadas){
+	for (size_t i = 0; i < coordenadas.size(); i++){
+		Coordenada coord = coordenadas[i];
+		size_t x = coord.obtener_ordenada();
+		size_t y = coord.obtener_abscisa()
+		celdas[x][y] = new Celda_aire(elem);
 	}
 }
 
-/*Ubicable& Mapa::obtener_ubicable(Coordenada &coordenada){
-	size_t x = coordenada.obtener_abscisa();
-	size_t y = coordenada.obtener_ordenada();
-	Celda posicion = celdas[x][y];
-	return posicion.obtener_ubicable();
-}*/
+void Mapa::ocupar_tierra(std::vector<Coordenada> coordenadas){
+	for (size_t i = 0; i < coordenadas.size(); i++){
+		Coordenada coord = coordenadas[i];
+		size_t x = coord.obtener_ordenada();
+		size_t y = coord.obtener_abscisa()
+		celdas[x][y] = new Celda_aire();
+	}
+}
 
-/*bool Mapa::coordenada_es_ocupable(Coordenada &coordenada){
-	size_t x = coordenadas[pos].obtener_abscisa();
-	size_t y = coordenadas[pos].obtener_ordenada();
-	return celdas[x][y].esta_ocupada();
-}*/
+void Mapa::cargar(){
+	std::vector<Coordenada> tierras;
+	std::vector<Coordenada> puas;
+	std::vector<Coordenada> escaleras;
+
+	for (size_t i = 0; i < 5; i++){
+		tierras.push_back(Coordenada(i,9));
+		tierras.push_back(Coordenada(i,1));
+	}
+	tierras.push_back(Coordenada(3,8));
+	for (i = 4, i < 10; i++){
+		tierras.push_back(Coordenada(i,7));
+		tierras.push_back(Coordenada(i,4));
+	}
+	tierras.push_back(Coordenada(1,4));
+	tierras.push_back(Coordenada(2,4));
+
+	puas.push_back(Coordenada(6,6));
+	puas.push_back(Coordenada(9,6));
+	puas.push_back(Coordenada(6,3));
+
+	escaleras.push_back(Coordenada(8,6));
+	escaleras.push_back(Coordenada(8,5));
+	escaleras.push_back(Coordenada(4,3));
+	escaleras.push_back(Coordenada(4,2));
+}
