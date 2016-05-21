96926a9933411c2c5a683e13863a850d8dd01e7a
Ana Czarnitzki <czarniana@gmail.com>
2016-05-21 18:12:31 -0300

    Terminé el cargar del mapa

--- src/Servidor/mapa.cpp
+++ src/Servidor/mapa.cpp
@@ -6,6 +6,55 @@
 #include "puas.h"
 #include "escalera.h"
 
+//-------------->Auxiliares<-----------//
+std::vector<Coordenada> coord_tierras(){
+	std::vector<Coordenada> tierras;
+
+	for (size_t i = 0; i < 5; i++){
+		tierras.push_back(Coordenada(i,9));
+		tierras.push_back(Coordenada(i,1));
+	}
+	tierras.push_back(Coordenada(0,8));
+	tierras.push_back(Coordenada(1,8));
+	tierras.push_back(Coordenada(0,7));
+	tierras.push_back(Coordenada(4,8));
+
+	for (i = 5, i < 10; i++){
+		tierras.push_back(Coordenada(i,7));
+		tierras.push_back(Coordenada(i,4));
+	}
+	for(i = 1; i < 4; i++){
+		tierras.push_back(Coordenada(i,4));
+	}
+
+	return tierras;
+}
+
+std::vector<Coordenada> coord_puas(){
+	std::vector<Coordenada> puas;
+
+	puas.push_back(Coordenada(2,0));
+	puas.push_back(Coordenada(1,3));
+	puas.push_back(Coordenada(6,3));
+
+	return puas;
+}
+
+std::vector<Coordenada> coord_escaleras(){
+	std::vector<Coordenada> escaleras;
+
+	for (i = 0; i < 3; i++){
+		escaleras.push_back(Coordenada(0,i+4));
+		escaleras.push_back(Coordenada(5,i+1));
+	}
+
+	return escaleras;
+}
+
+//------------------------------------//
+
+
+
 Mapa::Mapa(size_t tamanio){
 	tam = tamanio;
   mapa.cargar();
@@ -29,29 +78,26 @@ void Mapa::ocupar_tierra(std::vector<Coordenada> coordenadas){
 	}
 }
 
+void Mapa::rellenar_aire(){
+	for (size_t i = 0; i < tam; i++){
+		for (size_t j = 0; j < tam; j++){
+			if (!celdas[i][j]){
+				celdas[i][j] = new Celda_aire();
+			}
+		}
+	}
+}
+
 void Mapa::cargar(){
-	std::vector<Coordenada> tierras;
-	std::vector<Coordenada> puas;
-	std::vector<Coordenada> escaleras;
+	std::vector<Coordenada> tierras = coord_tierras();
+	std::vector<Coordenada> puas = coord_puas();
+	std::vector<Coordenada> escaleras = coord_escaleras();
 
-	for (size_t i = 0; i < 5; i++){
-		tierras.push_back(Coordenada(i,9));
-		tierras.push_back(Coordenada(i,1));
-	}
-	tierras.push_back(Coordenada(3,8));
-	for (i = 4, i < 10; i++){
-		tierras.push_back(Coordenada(i,7));
-		tierras.push_back(Coordenada(i,4));
-	}
-	tierras.push_back(Coordenada(1,4));
-	tierras.push_back(Coordenada(2,4));
+	Puas pinches;
+	Escalera esc;
 
-	puas.push_back(Coordenada(6,6));
-	puas.push_back(Coordenada(9,6));
-	puas.push_back(Coordenada(6,3));
-
-	escaleras.push_back(Coordenada(8,6));
-	escaleras.push_back(Coordenada(8,5));
-	escaleras.push_back(Coordenada(4,3));
-	escaleras.push_back(Coordenada(4,2));
+	ocupar_tierra(tierras);
+	ocupar_elemento(pinches, puas);
+	ocupar_elemento(es, escaleras);
+	rellenar_aire();
 }
