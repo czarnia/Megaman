62c76e7ab000ef99eaa726cdd99cf16638e1b99e
Ana Czarnitzki <czarniana@gmail.com>
2016-05-22 11:54:44 -0300

    Moví unos tabs y...bueno

--- src/Servidor/personaje.cpp
+++ src/Servidor/personaje.cpp
@@ -7,10 +7,9 @@ bool Personaje::ubicar(Mapa &mapa){
 void Personaje::mover(Mapa &mapa, StrategyMover &accion_mover){
 	//Dentro del accion_mover hay que checkear si se puede actualizar
 	//cada coordenada que se quiere actualizar
-	//verificando si el objeto se esta "moviendo sobre su yo anterior" si es que el personaje 
+  //verificando si el objeto se esta "moviendo sobre su yo anterior" si es que el personaje
 	//ocupa mas de una celda:
-	//if (accion_mover.mover(mapa, coordenadas_ocupadas)){ 
+	//if (accion_mover.mover(mapa, coordenadas_ocupadas)){
 		//this->ubicar(mapa, coordenadas_ocupadas); //Se actualiza el mapa.
 	//}
 }
-
