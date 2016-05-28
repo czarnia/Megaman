cf000cba70881bf3f242823b6fa4d1a530a72393
Ana Czarnitzki <czarniana@gmail.com>
2016-05-28 11:11:43 -0300

    Borro código usado para probar, defino los métodos que usará juego y dejo su código comentado a falta de mapa.

--- src/Servidor/juego.cpp
+++ src/Servidor/juego.cpp
@@ -8,4 +8,32 @@ Juego::Juego(size_t tamanio) : mundo(tamanio){}
 
 Juego::~Juego(){}
 
-void Juego::update(size_t tiempo){}
+void Juego::update(size_t tiempo){
+  /*
+  std::vector<Actualizable*> actualizables = mundo.obtener_actualizables();
+  for (size_t i = 0; i < actualizables.size(); i++){
+    actualizables[i]->update(tiempo);
+  }
+  */
+}
+
+void Juego::personaje_atacar(std::string id_pj, StrategyMover* direccion){
+  /*
+  Personaje* pj = mundo.obtener_pj(id_pj);
+  personaje.atacar(mundo, direccion);
+  */
+}
+
+void Juego::personaje_mover(std::string id_pj, StrategyMover* direccion){
+  /*
+  Personaje* pj = mundo.obtener_pj(id_pj);
+  personaje.mover(mundo, direccion);
+  */
+}
+
+void Juego::personaje_cambiar_arma(std::string id_pj, int arma){
+  /*
+  Personaje* pj = mundo.obtener_pj(id_pj);
+  personaje.cambiar_arma(arma);
+  */
+}
