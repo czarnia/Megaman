f846f2bbba508280a6ec9af3fcdb056a59efa071
Ana Czarnitzki <czarniana@gmail.com>
2016-06-04 10:18:33 -0300

    Borro lo repetido

--- src/Servidor/arma_met.cpp
+++
@@ -1,8 +0,0 @@
-#include "arma_met.h"
-#include "bala_met.h"
-
-Bala* Arma_met::atacar(int dir_x, int dir_y, Coordenada c) {
-  return new Bala_met(dir_x, dir_y, c);
-}
-
-Arma_met::~Arma_met() {}
--- src/Servidor/manejador_eventos.cpp
+++
@@ -1,9 +0,0 @@
-#include "manejador_eventos.h"
-
-Manejador_eventos::Manejador_eventos(Juego *m) : mundo(m){}
-
-Manejador_eventos::~Manejador_eventos() {}
-
-void Manejador_eventos::ejecutar_evento(Evento* e){
-  e->ejecutar(mundo);
-}
