c661f9d069f1f0781fcac5dee9308bcb9998ad6b
Ana Czarnitzki <czarniana@gmail.com>
2016-06-05 19:53:02 -0300

    Batton pass (2)

--- src/Editor/puas_factory.cpp
+++
@@ -1,9 +0,0 @@
-#include "puas_factory.h"
-#include "../Servidor/Modelo/puas.h"
-
-Puas_factory::Puas_factory() {}
-
-Ubicable* Puas_factory::crear(Mapa* mapa, Coordenada coord){
-  Puas* puas = new Puas(coord);
-  return puas;
-}
