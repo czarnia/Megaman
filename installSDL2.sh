a0da3dc4dcccf7b5d05060cdd627cbdb0e33dce8
emariotti3 <eugenia.mariotti@live.com.ar>
2016-06-06 20:31:05 -0300

    Modifico el bash para que instale sdl

--- installSDL2.sh
+++ installSDL2.sh
@@ -1,4 +1,11 @@
 apt-get install --yes build-essential xorg-dev libudev-dev libts-dev libgl1-mesa-dev libglu1-mesa-dev libasound2-dev libpulse-dev libopenal-dev libogg-dev libvorbis-dev libaudiofile-dev libpng12-dev libfreetype6-dev libusb-dev libdbus-1-dev zlib1g-dev libdirectfb-dev
+mkdir SDL
+cd SDL
+wget http://www.libsdl.org/release/SDL-1.12.15.tar.gz
+tar -xvzf SDL-1.12.15.tar.gz
+cd SDL-1.12.15
+./configure
+make
 mkdir SDL2
 cd SDL2
 wget http://www.libsdl.org/release/SDL2-2.0.4.tar.gz
@@ -22,9 +29,16 @@ cd SDL2_mixer-2.0.1
 make
 make install
 cd ..
-wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
-tar -xvzf SDL2_ttf-2.0.14.tar.gz
-cd SDL2_ttf-2.0.14
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11.tar.gz
+tar -xvzf SDL_ttf-2.0.11.tar.gz
+cd SDL_ttf-2.0.11
+./configure
+make
+make install
+cd ..
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
+tar -xvzf SDL2_ttf-2.0.12.tar.gz
+cd SDL2_ttf-2.0.12
 ./configure
 make
 make install
