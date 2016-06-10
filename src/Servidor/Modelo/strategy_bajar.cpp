52b467b6aa632603513493703b42ee5e1738028c
emariotti3 <eugenia.mariotti@live.com.ar>
2016-06-10 01:25:57 -0300

    Nuevo refactor strategies de movimiento. Agrego interaccion con escaleras.

--- src/Servidor/Modelo/strategy_bajar.cpp
+++
@@ -1,9 +0,0 @@
-#include "strategy_bajar.h"
-#include "coordenada.h"
-#include "mapa.h"
-void StrategyBajar::mover(Mapa *mapa, Personaje *pj){
-	Coordenada nueva_pos = pj->coordenada.abajo();
-	if (mapa->puede_ubicarse(pj, nueva_pos)){
-		pj->coordenada = nueva_pos;
-	}
-}
