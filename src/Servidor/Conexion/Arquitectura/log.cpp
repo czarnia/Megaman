0e037a8bd9506590c35501a3511ba809083e103c
Ana Czarnitzki <czarniana@gmail.com>
2016-06-11 11:33:44 -0300

    HABEMUS LOG, EL NOMBRE DEL USUARIO SE ESCRIBE MEDIO TURBIO, PERO HABEMUS LOG

--- src/Servidor/Conexion/Arquitectura/log.cpp
+++ src/Servidor/Conexion/Arquitectura/log.cpp
@@ -2,13 +2,14 @@
 #include "../../../Comun/lock.h"
 #include <ctime>
 #include <algorithm>
+#include <sstream>
 
 bool Log::hay_instancia = false;
 Log* Log::singleton = NULL;
 
 //-------------------->Auxiliares<--------------------//
 
-const char* obtener_nombre_log(){
+std::string obtener_nombre_log(){
 	//std::string fin_arch = ".txt";
 
 	std::time_t fecha = std::time(NULL);
@@ -16,7 +17,7 @@ const char* obtener_nombre_log(){
 	std::replace(fecha_str.begin(), fecha_str.end(), ' ', '-');
 	fecha_str.append(".txt");
 
-	return fecha_str.c_str();
+	return fecha_str;
 }
 
 //----------------------------------------------------//
@@ -35,93 +36,90 @@ Log::Log(){
 }
 
 Log::~Log(){
-	//this->log_arch.close();
+	//exportar(str.str());
 	singleton = NULL;
 	hay_instancia = false;
 }
 
 void Log::inicio_servidor(){
 	std::cout << "------------------INICIO SERVIDOR--------------\n";
-	Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	this->log_arch << "Servidor inicia ejecucion." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Servidor inicia ejecucion.";
+	exportar(str.str());
 }
 
 void Log::fin_servidor(){
-	Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	this->log_arch << "Servidor finaliza ejecucion." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Servidor finaliza ejecucion.";
+	exportar(str.str());
 }
 
 void Log::cliente_conectado(std::string nombre_cliente){
 	std::cout << "------------------CLIENTE CONECTADO--------------\n";
-  Lock candado(proteccion);
 	jugadores[jugadores.size()+1] = nombre_cliente;
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	this->log_arch << "Cliente " << nombre_cliente << " se ha conectado." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Cliente " << nombre_cliente << " se ha conectado.";
+	exportar(str.str());
 }
 
 void Log::cliente_desconectado(int id_cliente){
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	this->log_arch << "Cliente " << jugadores[id_cliente] << " se ha desconectado." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Cliente " << jugadores[id_cliente] << " se ha desconectado.";
+	exportar(str.str());
 }
 
 void Log::inicio_juego(){
 	std::cout << "------------------INICIO JUEGO--------------\n";
-	Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	this->log_arch << "Se inicia el juego con " << jugadores.size() << " jugadores." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Se inicia el juego con " << jugadores.size() << " jugadores.";
+	exportar(str.str());
 }
 
 void Log::fin_juego(){
-	Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-	log_arch << "Fin del juego." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Fin del juego." << std::endl;
+	exportar(str.str());
 }
 
 void Log::inicio_nivel(int num_nivel){
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-  log_arch << "Se inicia el nivel " << num_nivel << "." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+  str << "Se inicia el nivel " << num_nivel << ".";
+	exportar(str.str());
 }
 
 void Log::fin_nivel(){
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-  log_arch << "Se finalizo el nivel." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+	str << "Se finalizo el nivel." << std::endl;
+	exportar(str.str());
 }
 
 void Log::jugador_atacar(int id_cliente, int direccion){
 	std::cout << "------------------JUGADOR ATACAR--------------\n";
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-  log_arch << "El jugador " << jugadores[id_cliente] << " ataca en direccion "
-  << direccion << "." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+  str << "El jugador " << jugadores[id_cliente] << " ataca en direccion "
+  << direccion << ".";
+	exportar(str.str());
 }
 
 void Log::jugador_mover(int id_cliente, int direccion){
 	std::cout << "------------------JUGADOR MOVER--------------\n";
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-  log_arch << "El jugador " << jugadores[id_cliente] << " se mueve en direccion"
-  << " " << direccion << "." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+  str << "El jugador " << jugadores[id_cliente] << " se mueve en direccion"
+  << " " << direccion << ".";
+	exportar(str.str());
 }
 
 void Log::jugador_cambiar_arma(int id_cliente, int arma){
-  Lock candado(proteccion);
-	this->log_arch.open(path, std::ios::out | std::ios::app);
-  log_arch << "El jugador " << jugadores[id_cliente] << " cambia su arma por "
-  << arma << "." << std::endl;
-	this->log_arch.close();
+	std::stringstream str;
+  str << "El jugador " << jugadores[id_cliente] << " cambia su arma por "
+  << arma << ".";
+	exportar(str.str());
+}
+
+void Log::exportar(std::string mensaje){
+	Lock candado(proteccion);
+	std::ofstream log_arch;
+	log_arch.open(path.c_str(), std::fstream::app | std::fstream::out);
+	log_arch << mensaje << std::endl;
+	log_arch.close();
 }
