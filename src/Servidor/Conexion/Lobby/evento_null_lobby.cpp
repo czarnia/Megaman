2fa99ad225e742d01181d837bcc27db6d57b33a8
Ana Czarnitzki <czarniana@gmail.com>
2016-06-20 22:01:00 -0300

    Agrego los eventos null

---
+++ src/Servidor/Conexion/Lobby/evento_null_lobby.cpp
@@ -0,0 +1,5 @@
+#include "evento_null_lobby.h"
+#include "../Arquitectura/servidor.h"
+#include "../Arquitectura/log.h"
+
+void Evento_null_lobby::ejecutar(Servidor* s) {}
--- src/Servidor/Conexion/Partida/procesador_eventos.cpp
+++ src/Servidor/Conexion/Partida/procesador_eventos.cpp
@@ -3,6 +3,9 @@
 #include "creador_mover.h"
 #include "creador_parar.h"
 #include "creador_cambiar_arma.h"
+
+#include "evento_null_partida.h"
+
 #include <vector>
 #include <string>
 
@@ -11,9 +14,11 @@ Procesador_eventos::Procesador_eventos(int id_procesador) : id(id_procesador){
 }
 
 Evento* Procesador_eventos::crear_evento(int comando, int parametro){
-  Evento* e = creadores[comando-1]->crear(parametro); //TODO: if comando-1
-  // < 0, lanzar excepcion.
-
+  if ((comando-1 > creadores.size()) || (comando-1 < 0)){
+    Evento* e = new Evento_null_partida();
+    return e;
+  }
+  Evento* e = creadores[comando-1]->crear(parametro);
   return e;
 }
 
